262e0187cd07b62cc5a7b3646d0a5c31e31a2df0
gonzaSala <gonzasalamendi@gmail.com>
2023-09-28 20:13:46 -0300

    agregamos lectura

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
+//SERVICIOS
+import 'package:crud/services/firebase_services.dart';
+//FIREBASE
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-
-//- Importamos Firebase
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,15 +20,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Mi Proyecto Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Mi Aplicacióndf Flutter'),
-        ),
-        body: Center(
-          child: Text('¡Hola, Flasdasduxzczxctter!'),
-        ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mi Aplicacióndf Flutter'),
+      ),
+      body: FutureBuilder(
+        future: getPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return Text(snapshot.data?[index]['name']);
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
---
+++ lib/services/firebase_services.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//- LEER DATOS DESDE FIRESTORE
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<List> getPeople() async {
+  List people = [];
+  CollectionReference collectionReferencePeople = db.collection('people');
+
+  QuerySnapshot queryPeople = await collectionReferencePeople.get();
+
+  queryPeople.docs.forEach((e) {
+    people.add(e.data());
+  });
+
+  return people;
+}
